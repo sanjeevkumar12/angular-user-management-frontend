31c5b5f0b679e980a1306678c67afa2a7bb0825f
sanjeevkumar12 <sanjumassal@gmail.com>
2021-08-16 14:27:10 +0530

    Base Setup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit , Renderer2} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { TokenStorageService } from '../../core/services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +11,38 @@ import { Component, OnInit , Renderer2} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private renderer: Renderer2) { 
+  form : FormGroup;
+  authError: string|null = null;
+
+  constructor(private renderer: Renderer2, private authService : AuthService,private formBuilder: FormBuilder,
+              private tokenService: TokenStorageService, private router: Router
+              ) { 
     this.renderer.addClass(document.body, 'app-login');
     this.renderer.addClass(document.body,'p-0')
+    this.form = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['']
+    });
   }
 
   ngOnInit(): void {
-   
+    
+  }
+
+  get f() { return this.form.controls; }
+
+  login(){
+    if(this.form.valid){
+      this.authService.login(this.f.email.value, this.f.password.value).subscribe((res) => {
+        this.tokenService.saveToken(res.token)
+        this.tokenService.saveUser(res.user)
+        this.router.navigate(['/'])
+      }, response => {
+        if(response.status==422){
+          this.authError = response.error.errors.auth_error;
+        }
+      });
+    }
   }
 
 }
