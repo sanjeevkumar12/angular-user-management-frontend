64aacce5d184589b093f47bd3db9cf14f3969131
sanjeevkumar12 <sanjumassal@gmail.com>
2021-08-16 11:51:17 +0530

    Base Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch:'full'
+  },
+  { path: 'dashboard',  loadChildren: () =>  import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    data: { showHeader: true, showSidebar: true , showFooter : true
+    }
+  },
+  {path: 'login', component: LoginComponent, data: { showHeader: false, showSidebar: false, showFooter : false}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit , Renderer2} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private renderer: Renderer2) { 
+    this.renderer.addClass(document.body, 'app-login');
+    this.renderer.addClass(document.body,'p-0')
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+}
